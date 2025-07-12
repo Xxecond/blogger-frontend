301649250a8cb0c47d9ce90f7b0a886469c8e161
xxecond <andrewsampadu9@gmail.com>
2025-07-12 10:17:50 +0000

    P Change 7

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -1,4 +1,4 @@
- import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSearchParams, useNavigate } from 'react-router-dom';
 
 function VerifyEmail() {
